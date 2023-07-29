1d90a1dc1bb5a261ba137923f657d30bd1d5eee9
Say Ryder <lindz.ryder@gmail.com>
2023-07-29 13:27:22 -0400

    add development and production uris

--- accounts/google_auth.py
+++ accounts/google_auth.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 # Load the environment variables
 load_dotenv()
 
+django_env = os.getenv('DJANGO_ENV', 'development')
+
+if django_env == 'production':
+    redirect_uri = 'https://skillfusion-db.onrender.com/oauth2callback/'
+else:
+    redirect_uri = 'http://localhost:8000/oauth2callback/'
+
 # Get the credentials from environment variables
 client_id = os.getenv("GOOGLE_CLIENT_ID")
 client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
@@ -12,4 +19,4 @@ client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
 flow = OAuth2WebServerFlow(client_id=client_id,
                            client_secret=client_secret,
                            scope='https://www.googleapis.com/auth/calendar',
-                           redirect_uri='http://localhost:8000/oauth2callback/')
+                           redirect_uri=redirect_uri)
