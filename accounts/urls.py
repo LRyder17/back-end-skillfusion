034fcb14084db09de0d30ddf437751e370aca5b9
Say Ryder <lindz.ryder@gmail.com>
2023-07-29 13:32:56 -0400

    update url for profile_list

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile_list/', views.profile_list, name="profile_list"),
+    path('skillfusion-db.onrender.com/profile_list/', views.profile_list, name="profile_list"),
     path('users/<int:user_id>/following/', views.following_list, name='following_list'),
     path('users/<int:user_id>/followers/', views.follower_list, name='follower_list'),
     # path('create_student/', views.create_student, name='create_student'),
