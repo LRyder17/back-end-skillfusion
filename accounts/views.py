077e941e1ff0517113a60bc2891c4723b3e4945f
Say Ryder <lindz.ryder@gmail.com>
2023-07-31 14:03:42 -0400

    make follow/unfollow button functional

--- accounts/views.py
+++ accounts/views.py
@@ -73,6 +73,19 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        #POST form logic
+        if request.method == "POST":
+            # Get current user id
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            #Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            else:
+                current_user_profile.follows.add(profile)
+            # Save state of profile
+            current_user_profile.save()
         return render(request, "profile.html", {"profile": profile})
     else:
         messages.success(request,("You must be logged in to view this page"))
